5f1a25831f5c1ca4aee266e29fcac76dd31fec05
Bassani <gabrielbassabarreto@gmail.com>
2022-02-13 22:18:57 -0300

    finalizacao de projeto

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,18 @@ const ShowModal = (openButton, modalContent) => {
 
   if (openBtn && modalContainer) {
     openBtn.addEventListener("click", () => {
-      modalContainer.classList("show-modal");
+      modalContainer.classList.add("show-modal");
     });
   }
 };
 
 ShowModal("open-modal", "modal-container");
+
+const closeBtn = document.querySelectorAll(".close-modal");
+
+function closeModal() {
+  const modalContainer = document.getElementById("modal-container");
+  modalContainer.classList.remove("show-modal");
+}
+
+closeBtn.forEach((c) => c.addEventListener("click", closeModal));
